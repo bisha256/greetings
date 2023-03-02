e6810c9876dea6722eba19e2d8d96dee8e10c050
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-02 17:55:02 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="display: flex; justify-content: space-between">
+    <ConfettiExplosion :particleCount="200" :force="0.3" />
+    <h1>What a great looking Header this is</h1>
+    <ConfettiExplosion :particleCount="200" :force="0.3" />
+  </div>
+</template>
+
+<script setup>
+import ConfettiExplosion from "vue-confetti-explosion";
+</script>
