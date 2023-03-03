22f13bde28ce0dea30add08e54d92eb4b1933893
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-03 23:58:03 +0200

    add random messages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div style="display: flex; justify-content: space-between">
     <ConfettiExplosion v-if="confetti" :particleCount="200" :force="0.3" />
-    <button @click="onClick" class="btn">Yay!</button>
+    <button @click="onClick" class="btn" v-if="!displayMessage">Yay!</button>
+    <p v-if="displayMessage" class="paragraph">{{ displayMessage }}</p>
     <ConfettiExplosion v-if="confetti" :particleCount="200" :force="0.3" />
   </div>
 </template>
@@ -12,10 +13,14 @@ import ConfettiExplosion from "vue-confetti-explosion";
 
 const audio = new Audio("/sound/children_yay_sound_effect.mp3");
 const confetti = ref(false);
+const messages = ["可愛すぎる", "おはよう", "一緒に働くことを楽しんでいます"];
+
+const displayMessage = ref("");
 
 const onClick = () => {
   audio.play();
   confetti.value = true;
+  displayMessage.value = messages[Math.floor(Math.random() * messages.length)];
 };
 </script>
 
@@ -24,5 +29,12 @@ const onClick = () => {
   display: block;
   background-color: #ffc0cb;
   color: white;
+  font-size: 1.25rem;
+}
+
+.paragraph {
+  font-weight: 600;
+  font-size: 3rem;
+  color: #ffc0cb;
 }
 </style>
