55772d8626d6adca2afcf0dc0ccdfd0f1cd8add9
Ahmed Bashir <bisha.mail.box@gmail.com>
2023-03-03 23:45:45 +0200

    add trigger button for confetti and sound effect

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div style="display: flex; justify-content: space-between">
-    <ConfettiExplosion :particleCount="200" :force="0.3" />
-    <h1>Sorry you caught cold because of me</h1>
-    <ConfettiExplosion :particleCount="200" :force="0.3" />
+    <ConfettiExplosion v-if="confetti" :particleCount="200" :force="0.3" />
+    <button @click="onClick" class="btn">Yay!</button>
+    <ConfettiExplosion v-if="confetti" :particleCount="200" :force="0.3" />
   </div>
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { ref } from "vue";
 import ConfettiExplosion from "vue-confetti-explosion";
 
 const audio = new Audio("/sound/children_yay_sound_effect.mp3");
+const confetti = ref(false);
 
-onMounted(async () => {
-  await audio.play();
-});
+const onClick = () => {
+  audio.play();
+  confetti.value = true;
+};
 </script>
+
+<style>
+.btn {
+  display: block;
+  background-color: #ffc0cb;
+  color: white;
+}
+</style>
